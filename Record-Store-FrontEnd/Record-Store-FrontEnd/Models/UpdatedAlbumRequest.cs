62495382fa030185e114cbc09b63240a79b136d5
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-05-18 12:36:51 +0100

    Added Record model and Record components to be able to see list of records available on the homepage

---
+++ Record-Store-FrontEnd/Record-Store-FrontEnd/Models/UpdatedAlbumRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Record_Store_FrontEnd.Models
+{
+    public class UpdatedAlbumRequest
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Artist { get; set; }
+        [Range(1900, 2100)]
+        public int ReleaseYear { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Range(0, 100)]
+        public int Stock { get; set; }
+    }
+}
